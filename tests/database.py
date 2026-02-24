d2e55f37923a01a3db211765fecabccfe761d7d7
Juliya <kjj.juliya@gmail.com>
2026-02-23 21:33:55 -0500

    changed database connection

--- tests/database.py
+++ tests/database.py
@@ -6,7 +6,6 @@ from fastapi import HTTPException
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.exc import SQLAlchemyError 
-from app.config import settings
 import pytest
 
 
