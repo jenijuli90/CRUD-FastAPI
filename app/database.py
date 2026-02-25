6af0d67f4229cb30279097a047d12c89f28beffd
Juliya <kjj.juliya@gmail.com>
2026-02-24 20:19:29 -0500

    env change recent

--- app/database.py
+++ app/database.py
@@ -9,7 +9,7 @@ from .config import settings
 
 
 
-DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port/{settings.database_name}}'
+DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
 print (f"DATABASE_URL")
 
